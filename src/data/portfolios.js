3f21e3522f9b3afc6bbbff1cefdaf65bffdd15ac
GaryMcCulloch <garymcculloch1@gmail.com>
2021-10-29 11:05:34 +0100

    portfolio page updated

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,53 +1,53 @@
-import img1 from '../img/maya-1.jpg';
-import img2 from '../img/maya-4.jpg';
+import img1 from '../img/audiopunk.png';
+import img2 from '../img/cryptohound.png';
 import img3 from '../img/maya-3.jpg';
 
 const portfolios = [
     {
         id: 1,
-        category: 'Python',
+        category: 'Front-end',
         image: img1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Python',
-        text: 'Numper one Programming language'
+        
+        title: 'AudioPunk',
+        text: 'Application which connects with the users Spotify account to search songs, create playlists and more.',
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquid facilis sed molestiae nulla soluta accusantium dicta, laudantium animi temporibus, earum commodi eos ea, ducimus tenetur doloremque eaque nihil illum?",
     },
     {
         id: 2,
-        category: 'Python',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 3,
-        category: 'Javascript',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 4,
-        category: 'Animation',
+        category: 'Front-end',
         image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
+        title: 'CryptoHound',
+        text: 'This application keeps the users with the most recent market rate trends for Cryptocurrency',
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquid facilis sed molestiae nulla soluta accusantium dicta, laudantium animi temporibus, earum commodi eos ea, ducimus tenetur doloremque eaque nihil illum?",
+
     },
-    {
-        id: 5,
-        category: 'Animation',
-        image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
-    }
+    // {
+    //     id: 3,
+    //     category: 'Javascript',
+    //     image: img3,
+    //     link1: 'https://www.google.com',
+    //     link2: 'https://www.google.com',
+    //     title: 'How To Animate In Maya',
+    //     text: 'Number one Animation Application'
+    // },
+    // {
+    //     id: 4,
+    //     category: 'Animation',
+    //     image: img2,
+    //     link1: 'https://www.google.com',
+    //     link2: 'https://www.google.com',
+    //     title: 'How To Use Blender',
+    //     text: 'Free Animation Software'
+    // },
+    // {
+    //     id: 5,
+    //     category: 'Animation',
+    //     image: img2,
+    //     link1: 'https://www.google.com',
+    //     link2: 'https://www.google.com',
+    //     title: 'How To Use Blender',
+    //     text: 'Free Animation Software'
+    // }
 ]
 
 export default portfolios;
